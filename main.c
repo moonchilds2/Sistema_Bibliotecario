12fdc8096eeba485f15ac73b9b84d7fe663bf7f3
zhonyuwu <108583543+zhonyuwu@users.noreply.github.com>
2022-07-06 13:03:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -17,33 +17,14 @@ int main(){
         printf("Digite a funcionalidade desejada:");
         scanf("%d", &resp);
         if(resp == 1){
-            add_encomenda();
+            idaleatorio();
+            add_encomenda(i);
         }else if(resp == 2){
-            int retorno = verificar(cpf, senha); //verificar cpf e senha do secretario
-            remover_encomenda(); // chamar a funcao para REMOVER A ENCOMENDA se nao REMOVER PEDIDO
-            if(retorno == 1){
-                remover_encomenda();
-                add_dados();
-                add_fila();
-            }else if(resp == 3){               
-                //verificar_usuario();
-                    printf(" Digite seu cpf:\n");
-                    char cpf[100];
-                    scanf("%s", &cpf);
-                    printf(" Digite sua senha:\n");
-                    char senha[100];
-                    scanf("%s", &senha);
-                    //int retorno = verificar(cpf, senha);
-                    if(retorno == 1){
-                        remover_fila();
-                        //remover da fila de prioridade
-                    }
-            }
-        
-        }*/
-        
+            validacao();
+         }else if(resp == 3){               
+             validacao();
+         }
     }
     
     return 0;
 }
-Footer
