a60411268d073d28ecbd8bc4e70756bd0a92021f
Maria Eduarda Oliveira <84726659+eduarda-ios@users.noreply.github.com>
2022-07-06 11:07:36 -0300

    Add files via upload
    Fila: adicionar já feito e uma tentativa de remover na fila.

---
+++ fila_add_remover.h
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct pedido{
+    int identificador, matricula;
+    char * campus_onde_esta_o_livro;
+    char * campus_onde_esta_o_aluno;
+    char * nome_do_aluno;
+    char * nome_do_livro;
+    char * nome_do_autor;
+    int nivel_propriedade;
+
+    DADOS_TRANSPORTADOR * d;
+    struct pedido * prox;
+}pedido;
+
+typedef struct dados_transportador{
+    char * nome_transportador;
+    long long cpf;
+    int id_transportador; // verificar 
+}DADOS_TRANSPORTADOR;
+
+pedido * inicio_f = NULL;
+pedido * fim_f = NULL;
+int tam = 0;
+
+void add_na_lista(int identificador, int matricula, char * campus_onde_esta_o_livro, char * campus_onde_esta_o_aluno,
+                  char * nome_do_aluno, char * nome_do_livro, char * nome_do_autor, int nivel_propriedade){
+    pedido * novo_ped = (pedido *)malloc(sizeof(pedido));
+    novo_ped -> identificador = identificador;
+    novo_ped -> matricula = matricula;
+    novo_ped -> campus_onde_esta_o_livro = campus_onde_esta_o_livro;
+    novo_ped -> campus_onde_esta_o_aluno = campus_onde_esta_o_aluno;
+    novo_ped -> nome_do_aluno = nome_do_aluno;
+    novo_ped -> nome_do_livro = nome_do_livro;
+    novo_ped -> nome_do_autor = nome_do_autor;
+    novo_ped -> nivel_propriedade = nivel_propriedade;
+
+    if(inicio_f == NULL){
+        inicio_f = novo_ped;
+        fim_f = novo_ped;
+    }else if(inicio_f -> nivel_propriedade < novo_ped -> nivel_propriedade){
+        novo_ped -> prox = inicio_f;
+        inicio_f = novo_ped;
+    }else if(fim_f -> nivel_propriedade > novo_ped -> nivel_propriedade){
+        fim_f -> prox = novo_ped;
+        fim_f = novo_ped;
+    }else{
+        pedido * aux = inicio_f;
+        while(aux -> nivel_propriedade > novo_ped -> nivel_propriedade){
+            aux = aux -> prox;
+        }
+        novo_ped -> prox = aux -> prox;
+        aux -> prox = novo_ped;
+    }
+    tam++;
+
+}
+
+void add_transportador(char * nome_transportador, long long cpf, int id_transportador){   
+       
+        DADOS_TRANSPORTADOR * d = (DADOS_TRANSPORTADOR*)malloc(sizeof(DADOS_TRANSPORTADOR));
+        d->nome_transportador = nome_transportador;
+        d->cpf = cpf;
+        d->id_transportador = id_transportador;
+
+        pedido * novo = (pedido *)malloc(sizeof(pedido));
+        novo-> p = p;
+        novo->prox = NULL;
+   
+   
+        if(inicio_f == NULL){
+            inicio_f = novo;
+            fim_f = novo;
+            tam++;
+        }else{           
+            fim_f->prox = novo;
+            fim_f = novo;
+            tam++;
+        }
+}
+
+
+
+pedido remover_fila(){
+    pedido pedido;
+        if(inicio_f != NULL){  
+            pedido * lixo = inicio_f;
+            inicio_f = inicio_f -> prox;
+            pedido.identificador = lixo->p->identificador;
+
+            free(lixo);
+            tam--;
+            if(tam == 1){
+                fim_f = NULL;
+            }
+        printf("Nome do TRANSPORTADOR responsavel: %d\n", aux->d->nome_transportador);                   
+        }         
+    return pedido;
+}
